8e3b1e6bf65325f9e22dbe4b4f651b574ee5eea6
HaiderAlboloushi180 <halbolou05@gmail.com>
2022-03-28 09:42:33 +0300

    added all disorders

--- app/src/main/java/com/example/mentapy/CommonIllnessesActivity.java
+++ app/src/main/java/com/example/mentapy/CommonIllnessesActivity.java
@@ -2,7 +2,10 @@ package com.example.mentapy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,7 +19,7 @@ public class CommonIllnessesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_illnesses);
 
-        // Array Stuff
+        // Objects
         MentalDisorder depression = new MentalDisorder(
                 getString(R.string.depression),
                 getString(R.string.depressionDesc),
@@ -24,14 +27,7 @@ public class CommonIllnessesActivity extends AppCompatActivity {
                         getString(R.string.depressionSymptom2) +
                         getString(R.string.depressionSymptom3) +
                         getString(R.string.depressionSymptom4) +
-                        getString(R.string.depressionSymptom5) +
-                        getString(R.string.depressionSymptom6) +
-                        getString(R.string.depressionSymptom7) +
-                        getString(R.string.depressionSymptom8) +
-                        getString(R.string.depressionSymptom9) +
-                        getString(R.string.depressionSymptom10) +
-                        getString(R.string.depressionSymptom11) +
-                        getString(R.string.depressionSymptom12),
+                        getString(R.string.depressionSymptom5) ,
                 getString(R.string.depressionTip1) +
                         getString(R.string.depressionTip2) +
                         getString(R.string.depressionTip3) +
@@ -44,7 +40,7 @@ public class CommonIllnessesActivity extends AppCompatActivity {
                 getString(R.string.ptsdSymptom1) +
                         getString(R.string.ptsdSymptom2) +
                         getString(R.string.ptsdSymptom3) +
-                        getString(R.string.ptsdSymptom4),
+                        getString(R.string.ptsdSymptom4) ,
                 getString(R.string.ptsdTip1) +
                         getString(R.string.ptsdTip2) +
                         getString(R.string.ptsdTip3),
@@ -74,18 +70,58 @@ public class CommonIllnessesActivity extends AppCompatActivity {
                 getString(R.string.bipolarTip1) +
                         getString(R.string.bipolarTip2) +
                         getString(R.string.bipolarTip3),
-                R.drawable.ocd
+                R.drawable.bipolar
+        );
+        MentalDisorder schizophrenia = new MentalDisorder(
+                getString(R.string.schizo),
+                getString(R.string.schizoDesc),
+                getString(R.string.schizoSymptom1) +
+                        getString(R.string.schizoSymptom2) +
+                        getString(R.string.schizoSymptom3) +
+                        getString(R.string.schizoSymptom4) +
+                        getString(R.string.schizoSymptom5) ,
+                getString(R.string.schizoTip1) +
+                        getString(R.string.schizoTip2) +
+                        getString(R.string.schizoTip3),
+                R.drawable.schizo
+        );
+        MentalDisorder addiction = new MentalDisorder(
+                getString(R.string.addiction),
+                getString(R.string.addiction),
+                getString(R.string.addictionSymptom1) +
+                        getString(R.string.addictionSymptom2) +
+                        getString(R.string.addictionSymptom3) +
+                        getString(R.string.addictionSymptom4) +
+                        getString(R.string.addictionSymptom5) ,
+                getString(R.string.addictionTip1) +
+                        getString(R.string.addictionTip2) +
+                        getString(R.string.addictionTip3),
+                R.drawable.addiction
         );
 
+        // Adding to array
         disorders.add(depression);
         disorders.add(ptsd);
         disorders.add(ocd);
+        disorders.add(bipolar);
+        disorders.add(schizophrenia);
+        disorders.add(addiction);
 
         // ListView and Adapter stuff
         ListView disordersListView = findViewById(R.id.disordersListView);
 
         MentalDisorderAdapter disorderAdapter = new MentalDisorderAdapter(this, 0, disorders);
         disordersListView.setAdapter(disorderAdapter);
+
+        disordersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                MentalDisorder currentItem = disorders.get(i);
+                Intent intent = new Intent(CommonIllnessesActivity.this, CommonIllnessesDetails.class);
+                intent.putExtra("DISORDER", currentItem);
+                startActivity(intent);
+            }
+        });
 
     }
 }
