4b5b9fd6bcfa2491040714b42277e6dcfeb75760
HaiderAlboloushi180 <halbolou05@gmail.com>
2022-04-01 19:12:26 +0100

    done with if statements, almost done with feature

--- app/src/main/java/com/example/mentapy/QuizModel.java
+++ app/src/main/java/com/example/mentapy/QuizModel.java
@@ -6,13 +6,21 @@ public class QuizModel {
     private String option2;
     private String option3;
     private String option4;
+    private int option1Sev;
+    private int option2Sev;
+    private int option3Sev;
+    private int option4Sev;
 
-    public QuizModel(String question, String option1, String option2, String option3, String option4) {
+    public QuizModel(String question, String option1, String option2, String option3, String option4, int option1Sev, int option2Sev, int option3Sev, int option4Sev) {
         this.question = question;
         this.option1 = option1;
         this.option2 = option2;
         this.option3 = option3;
         this.option4 = option4;
+        this.option1Sev = option1Sev;
+        this.option2Sev = option2Sev;
+        this.option3Sev = option3Sev;
+        this.option4Sev = option4Sev;
     }
 
     public String getQuestion() { return question; }
@@ -29,4 +37,16 @@ public class QuizModel {
 
     public String getOption4() { return option4; }
     public void setOption4(String option4) { this.option4 = option4; }
+
+    public int getOption1Sev() { return option1Sev; }
+    public void setOption1Sev(int option1Sev) { this.option1Sev = option1Sev; }
+
+    public int getOption2Sev() { return option2Sev; }
+    public void setOption2Sev(int option2Sev) { this.option2Sev = option2Sev; }
+
+    public int getOption3Sev() { return option3Sev; }
+    public void setOption3Sev(int option3Sev) { this.option3Sev = option3Sev; }
+
+    public int getOption4Sev() { return option4Sev; }
+    public void setOption4Sev(int option4Sev) { this.option4Sev = option4Sev; }
 }
--- app/src/main/java/com/example/mentapy/SeverityTestActivity.java
+++ app/src/main/java/com/example/mentapy/SeverityTestActivity.java
@@ -2,6 +2,7 @@ package com.example.mentapy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,41 +15,83 @@ import java.util.Random;
 
 public class SeverityTestActivity extends AppCompatActivity {
 
-    // Views
-    private TextView questionCount = findViewById(R.id.questionCount);
-    private TextView questionText  = findViewById(R.id.question);
-    private Button option1Btn = findViewById(R.id.option1);
-    private Button option2Btn = findViewById(R.id.option2);
-    private Button option3Btn = findViewById(R.id.option3);
-    private Button option4Btn = findViewById(R.id.option4);
-    ArrayList<QuizModel> quizModelArrayList = new ArrayList<>();
-    private int questionsDid = 1, currentPos;
+    private int questionsDid = 1;
+    private int severity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_severity_test);
 
-        // Getting and setting the questions
-        getQuizQuestions(quizModelArrayList);
-        currentPos = new Random().nextInt(quizModelArrayList.size());
-        setDataToView(currentPos);
+        // Views
+        TextView questionCount = findViewById(R.id.questionCount);
+        TextView questionText  = findViewById(R.id.question);
+        Button option1Btn = findViewById(R.id.option1);
+        Button option2Btn = findViewById(R.id.option2);
+        Button option3Btn = findViewById(R.id.option3);
+        Button option4Btn = findViewById(R.id.option4);
+
+        // Questions
+        QuizModel q1 = new QuizModel("", "","", "", "", 20,0,0,0);
+        QuizModel q2 = new QuizModel("", "","", "", "", 0,0,0,0);
+        QuizModel q3 = new QuizModel("", "","", "", "", 0,0,0,0);
+        QuizModel q4 = new QuizModel("", "","", "", "", 0,0,0,0);
+        QuizModel q5 = new QuizModel("", "","", "", "", 0,0,0,0);
+
+        // Setting the views
+        questionCount.setText(questionsDid+"");
+        questionText.setText(q1.getQuestion());
+        option1Btn.setText(q1.getOption1());
+        option2Btn.setText(q1.getOption2());
+        option3Btn.setText(q1.getOption3());
+        option4Btn.setText(q1.getOption4());
+
+        option1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (questionText.getText() == q1.getQuestion()) {
+                    severity = severity + q1.getOption1Sev();
+                    questionsDid++;
+                    questionCount.setText(questionsDid+"");
+                    questionText.setText(q2.getQuestion());
+                    option1Btn.setText(q2.getOption1());
+                    option2Btn.setText(q2.getOption2());
+                    option3Btn.setText(q2.getOption3());
+                    option4Btn.setText(q2.getOption4());
+                } else if (questionText.getText() == q2.getQuestion()) {
+                    severity = severity + q2.getOption1Sev();
+                    questionsDid++;
+                    questionCount.setText(questionsDid+"");
+                    questionText.setText(q3.getQuestion());
+                    option1Btn.setText(q3.getOption1());
+                    option2Btn.setText(q3.getOption2());
+                    option3Btn.setText(q3.getOption3());
+                    option4Btn.setText(q3.getOption4());
+                } else if (questionText.getText() == q3.getQuestion()) {
+                    severity = severity + q3.getOption1Sev();
+                    questionsDid++;
+                    questionCount.setText(questionsDid+"");
+                    questionText.setText(q4.getQuestion());
+                    option1Btn.setText(q4.getOption1());
+                    option2Btn.setText(q4.getOption2());
+                    option3Btn.setText(q4.getOption3());
+                    option4Btn.setText(q4.getOption4());
+                } else if (questionText.getText() == q4.getQuestion()) {
+                    severity = severity + q4.getOption1Sev();
+                    questionsDid++;
+                    questionCount.setText(questionsDid+"");
+                    questionText.setText(q5.getQuestion());
+                    option1Btn.setText(q5.getOption1());
+                    option2Btn.setText(q5.getOption2());
+                    option3Btn.setText(q5.getOption3());
+                    option4Btn.setText(q5.getOption4());
+                } else if (questionText.getText() == q5.getQuestion()) {
+                    severity = severity + q4.getOption1Sev();
+                    Intent intent = new Intent(SeverityTestActivity.this, TestDetails.class);
+                    intent.putExtra("BAGS", severity);
+                }
+            }
+        });
     }
 
-    private void setDataToView(int currentPos) {
-        questionCount.setText("Question Number: "+currentPos+"/5");
-        questionText.setText(quizModelArrayList.get(currentPos).getQuestion());
-        option1Btn.setText(quizModelArrayList.get(currentPos).getOption1());
-        option2Btn.setText(quizModelArrayList.get(currentPos).getOption2());
-        option3Btn.setText(quizModelArrayList.get(currentPos).getOption3());
-        option4Btn.setText(quizModelArrayList.get(currentPos).getOption4());
-    }
-
-    private void getQuizQuestions(ArrayList<QuizModel> quizModelArrayList) {
-        quizModelArrayList.add(new QuizModel("Have you been able to do the stuff you enjoy doing?", "Yes, I am very productive", "Kinda, I am tired most of the time", "No but I want to be able to do em", "I have lost interest in the stuff that I used to do"));
-        quizModelArrayList.add(new QuizModel("How long do you usually sleep?", "I am not so sure", "Very little, 1-5 hours", "About 6-10 hours", "Too much 11+ hours"));
-        quizModelArrayList.add(new QuizModel("Have you been able to do the stuff you enjoy doing?", "Yes, I am very productive", "Kinda, I am tired most of the time", "No but I want to be able to do em", "I have lost interest in the stuff that I used to do"));
-        quizModelArrayList.add(new QuizModel("Have you been able to do the stuff you enjoy doing?", "Yes, I am very productive", "Kinda, I am tired most of the time", "No but I want to be able to do em", "I have lost interest in the stuff that I used to do"));
-        quizModelArrayList.add(new QuizModel("Have you been able to do the stuff you enjoy doing?", "Yes, I am very productive", "Kinda, I am tired most of the time", "No but I want to be able to do em", "I have lost interest in the stuff that I used to do"));
-    }
 }
