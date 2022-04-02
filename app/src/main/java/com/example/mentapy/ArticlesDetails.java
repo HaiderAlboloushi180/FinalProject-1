db1ff4cdbb10451a4a961c8878ded546bfb83621
HaiderAlboloushi180 <halbolou05@gmail.com>
2022-04-02 19:18:47 +0100

    DONEEEEEE!!!!

--- app/src/main/java/com/example/mentapy/ArticlesDetails.java
+++ app/src/main/java/com/example/mentapy/ArticlesDetails.java
@@ -2,7 +2,10 @@ package com.example.mentapy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,5 +24,19 @@ public class ArticlesDetails extends AppCompatActivity {
         TextView articleTitleTextView = findViewById(R.id.articleDetailsTitle);
         TextView articleDescrTextView = findViewById(R.id.articleDetailsDesc);
         Button visitButton = findViewById(R.id.articleButton);
+
+        articleTitleTextView.setText(selectedArticle.getArticleTitle());
+        articleDescrTextView.setText(selectedArticle.getArticleDesc());
+
+        // VISIT THE LINK
+        visitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = selectedArticle.getArticleLink();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
